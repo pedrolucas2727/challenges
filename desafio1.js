07c9eb4dcdf05a1892da1f1d281cfc7c744b52d2
pedro lucas <2003plco@gmail.com>
2024-08-21 09:28:43 -0300

    feat: new resolution

--- desafio1.js
+++ desafio1.js
@@ -1,11 +1,10 @@
-const F = 65
-const C = 30 
+let F = 30
+let C = 30 
 
-const tempF = function(){
-    return (C*9/5)+32
-}
+const tempF = C => {return (C*(9/5)+32)}
 const tempC = function(){
-    return (f-32)*9/5
+    return (F-32)*9/5
 }
-console.log(tempF)
-console.log(tempC)
+
+console.log(tempC(F))
+console.log(tempF(C))
