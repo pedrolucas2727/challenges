9cfaea4c6b82f193cf60125c18f2129689b8d3ff
pedro lucas <2003plco@gmail.com>
2024-08-27 10:41:36 -0300

    feat: resolution code 3

---
+++ desafio3.js
@@ -0,0 +1,6 @@
+const pessoa = {
+    nome: 'pedro',
+    idade: 21,
+    cidade: 'Rio Claro'
+}
+console.log(`O ${pessoa.nome} tem a idade de ${pessoa.idade} e mora na cidade de ${pessoa.cidade}!`)
